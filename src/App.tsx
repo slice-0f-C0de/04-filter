abe8a7e68a941a699277b9b5183b2675aa888686
slice_of_code <slice_of_code@gmail.com>
2023-02-13 23:19:44 +0400

    filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {FilteredMoney} from "./FilteredMoney";
+
+export type FilterType = "All" | "RUBLS" | "Dollars" // 7. Создаём тип для фильтров
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+
+    let [filter, setFilter] = useState<FilterType>("All") // 8. Типизируем стейт
+
+    let currentMoney = money
+
+    if (filter === "RUBLS") {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === "RUBLS")
+    }
+    if (filter === "Dollars") {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === "Dollars")
+    }
+
+    const onClickFilterHandler = (nameButton: FilterType) => { // 9. Типизируем название кнопки
+        setFilter(nameButton)
+    }
+
+    return (
+        <FilteredMoney currentMoney={currentMoney} onClickFilterHandler={onClickFilterHandler}/>
+    );
 }
 
 export default App;
